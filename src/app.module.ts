d1e97f35af38a67bbb33c001c29942e8afcc9fdc
Jang Yoonsung <upthe15752@gmail.com>
2022-12-30 17:23:40 +0900

    fix: import path 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigurationModule } from './config/configuration.module';
+import { ConfigurationModule } from '@config/configuration.module';
 
 @Module({
   imports: [ConfigurationModule],
--- src/config/decorator/custom-repository.decorator.ts
+++ src/config/decorator/custom-repository.decorator.ts
@@ -1,5 +1,5 @@
+import { TYPE_ORM_CUSTOM_REPOSITORY } from '@config/constants/config-const';
 import { SetMetadata } from '@nestjs/common';
-import { TYPE_ORM_CUSTOM_REPOSITORY } from '../constants/config-const';
 
 export const CustomRepository = <T>(entity: T): ClassDecorator =>
   SetMetadata(TYPE_ORM_CUSTOM_REPOSITORY, entity);
