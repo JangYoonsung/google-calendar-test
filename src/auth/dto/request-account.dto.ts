4e8e9bf37f5e243f324bbb5d30587e6da888eebf
Jang Yoonsung <upthe15752@gmail.com>
2022-12-30 17:07:39 +0900

    feat: update / login dto 작성

--- src/auth/dto/request-account.dto.ts
+++ src/auth/dto/request-account.dto.ts
@@ -1,3 +1,4 @@
+import { PartialType } from '@nestjs/mapped-types';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateAccountDto {
@@ -8,6 +9,18 @@ export class CreateAccountDto {
   @IsEmail()
   @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
+
+export class UpdateAccountDto extends PartialType(CreateAccountDto) {}
+
+export class RequestLoginDto {
+  @IsString()
+  @IsNotEmpty()
+  username: string;
 
   @IsString()
   @IsNotEmpty()
