c68f5cfc0a0f4f410031606b84f014c38b8860dd
Jang Yoonsung <upthe15752@gmail.com>
2023-01-01 20:19:45 +0900

    fix: 불필요한 파일 제거

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigurationModule } from '@config/configuration.module';
 import { AuthModule } from '@auth/auth.module';
 
 @Module({
   imports: [ConfigurationModule, AuthModule],
-  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
