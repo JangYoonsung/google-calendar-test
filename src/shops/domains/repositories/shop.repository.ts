6fc1f9c97fbfdd80dc2eb2a97e185859db51347c
Jang Yoonsung <upthe15752@gmail.com>
2023-01-03 20:35:01 +0900

    feat: getShopId추가

--- src/shops/domains/repositories/shop.repository.ts
+++ src/shops/domains/repositories/shop.repository.ts
@@ -4,6 +4,7 @@ import { Shop } from '@shops/infrastructure/entities';
 import { UpdateResult } from 'typeorm';
 
 export abstract class IShopRepository {
+  abstract getShopId(uuid: string): Promise<number>;
   abstract findById(id: string): Promise<ShopDomain>;
   abstract createShop(dto: CreateShopDto, accountId: number): Promise<Shop>;
   abstract updateShop(id: string, dto: UpdateShopDto): Promise<UpdateResult>;
